2ff1f7607f0694b8bd0b9de9f10ae72b502748c3
Vladislav Kulikov <v.kulikov@tg.dunice.net>
2023-10-24 15:45:50 +0300

    create httpexeption

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
 
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
-
 import { User } from '../../../database/models/user.model';
 import { Post } from '../../../database/models/post.model';
+
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,34 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 
 import { Post } from '../../../database/models/post.model';
 import { Tag } from '../../../database/models/tag.model';
 import { User } from '../../../database/models/user.model';
 
+import { errorMessages } from '../auth/constants/errorMessages';
+
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User) private readonly userInfo: typeof User) {}
 
   async getUserInfo(id: number) {
-    try {
-      const user = await this.userInfo.findOne({
-        where: { id },
-        attributes: ['email', 'login', 'avatarPath'],
-        include: [
-          {
-            model: Post,
-            include: [
-              {
-                model: Tag,
-                through: { attributes: [] },
-              },
-            ],
-          },
-        ],
-      });
+    const user = await this.userInfo.findOne({
+      where: { id },
+      attributes: ['email', 'login', 'avatarPath'],
+      include: [
+        {
+          model: Post,
+          include: [
+            {
+              model: Tag,
+              through: { attributes: [] },
+            },
+          ],
+        },
+      ],
+    });
+    if (user === null) {
+      throw new HttpException(
+        errorMessages.USER_DOES_NOT_EXIST,
+        HttpStatus.BAD_REQUEST,
+      );
+    } else {
       return user;
-    } catch (error) {
-      return error.message;
     }
   }
 }
